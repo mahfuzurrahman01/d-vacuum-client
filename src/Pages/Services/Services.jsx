016ecc88649b3dca3807dd23d26dffa54bd023b6
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-11-07 22:32:26 +0600

    layout and router done

---
+++ src/Pages/Services/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h1>THis is a service section</h1>
+        </div>
+    );
+};
+
+export default Services;
