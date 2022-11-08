084195ff35aa38e5208119cfb78b92fe5d1866bd
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-11-09 00:17:50 +0600

    login sign up auth decleared

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,11 +1,39 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import app from '../Firebase/Firebase.config';
-import { getAuth } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 const auth = getAuth(app);
 export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
-    const user = { name: 'mahfuz' };
-    const authInfo = { user }
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    //create user with email and password
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    //sign in user with email and password
+    const logInwithEmailandPassword = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    //log out 
+    const logOut = () => {
+        setLoading(true);
+        return signOut()
+    }
+
+    //on auth state change
+    useEffect(() => {
+        const subscription = onAuthStateChanged(auth, currentUser => {
+            console.log(currentUser)
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => subscription()
+    }, [])
+
+    const authInfo = { user, loading, createUser, logInwithEmailandPassword, logOut }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
