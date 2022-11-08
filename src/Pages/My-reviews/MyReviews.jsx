cad919e8e1a36d31efa0b28c1ed5f5a99563e4e0
mahfuzurrahman01 <mahfuzurrahman01010101@gmail.com>
2022-11-09 00:23:41 +0600

    my reviews and add service section added

---
+++ src/Pages/My-reviews/MyReviews.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyReviews = () => {
+    return (
+        <div>
+            <h1>my cart of review this one</h1>
+        </div>
+    );
+};
+
+export default MyReviews;
